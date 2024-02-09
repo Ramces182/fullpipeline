0fa5a377d9ae566770704f11fd21e3b1a7c94bd6
Daniel Muro <59543505+Ramces182@users.noreply.github.com>
2024-02-08 23:01:58 -0600

    initial test application

---
+++ src/main/java/com/fitness/cli/MainApp.java
@@ -0,0 +1,29 @@
+package com.fitness.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp
+        implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory.getLogger(MainApp.class);
+
+    public static void main(String[] args) {
+        LOG.info("STARTING THE APPLICATION");
+        SpringApplication.run(MainApp.class, args);
+        LOG.info("APPLICATION FINISHED");
+    }
+
+    @Override
+    public void run(String... args) {
+        LOG.info("EXECUTING : command line runner");
+
+        for (int i = 0; i < args.length; ++i) {
+            LOG.info("args[{}]: {}", i, args[i]);
+        }
+    }
+}
